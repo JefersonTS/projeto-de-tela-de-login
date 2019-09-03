1aea7c04bfc02d71a0a518a12fada245febc2b68
Jeferson Tomas <jeferson.tomas.santos@gmail.com>
2019-09-03 20:55:37 -0300

    Página de login com Icones

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,11 +5,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40
-        ),
+        padding: EdgeInsets.only(top: 60, left: 40, right: 40),
         color: Colors.white,
         child: ListView(
           children: <Widget>[
@@ -19,38 +14,73 @@ class LoginPage extends StatelessWidget {
               height: 128,
               child: Image.asset("assets/logo.png"),
             ),
-            SizedBox(
-              height: 20
-            ),
-            TextFormField(
-              autofocus: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
+            SizedBox(height: 20),
+            new ListTile(
+              leading: const Icon(
+                Icons.account_circle,
+                color: Colors.deepOrange,
+                size: 60.0,
               ),
-              style: TextStyle(
-                fontSize: 20,
+              title: new TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
               ),
             ),
             SizedBox(height: 10),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
+            new ListTile(
+              leading: const Icon(
+                Icons.vpn_key,
+                color: Colors.deepOrange,
+                size: 60.0,
               ),
-              style: TextStyle(fontSize: 20),
+              title: new TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+                style: TextStyle(fontSize: 20),
+              ),
             ),
+            /*Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.account_circle,
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+                TextFormField(
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    labelStyle: TextStyle(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),*/
           ],
         ),
       ),
